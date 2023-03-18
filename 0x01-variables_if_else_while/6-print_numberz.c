4e2cf5d3c6f571f55364f495e76cab2f2d2f9434
Dibuji <emekzy10@gmail.com>
2023-03-18 04:31:29 -0700

    Print numbers with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print single base 10 digits with putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+
+return (0);
+}
