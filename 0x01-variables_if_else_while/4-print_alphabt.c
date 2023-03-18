bcb1c1c7dd3099a0e7a38a72bf102a920ed119e0
Dibuji <emekzy10@gmail.com>
2023-03-18 03:39:32 -0700

    Excluding some letters from output

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Putchar to print out all letters with exceptions
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar(letter);
+		}
+		putchar('\n');
+	}
+	return (0)
+}
