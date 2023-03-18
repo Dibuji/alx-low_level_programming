802a3e424e66681860d96ee6a6a6e9489a732566
Dibuji <emekzy10@gmail.com>
2023-03-18 09:31:15 -0700

    print single digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,13 +11,13 @@ int main(void)
 
 	for (a = 0; a < 10; a++)
 	{
-		putchar(n + '0');
-		if (n != 9)
+		putchar(a + '0');
+		if (a != 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
-	putchar('\n')
+	putchar('\n');
 	return (0);
 }
