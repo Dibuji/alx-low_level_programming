6642454ff03e592385d95fe4ccc91db199b7ee3e
Dibuji <emekzy10@gmail.com>
2023-03-20 14:51:35 -0700

    standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - Print to standard error
+ * Return: 0
+ */
+
+int main(void)
+{
+	const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, message, 38);
+	return (1);
+}
