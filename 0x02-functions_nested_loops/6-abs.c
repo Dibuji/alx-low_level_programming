b179ff0c2fe5a3c4300482f2e774376169497e7c
Dibuji <emekzy10@gmail.com>
2023-03-22 15:56:43 -0700

    l

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - compute absolute value
+ * Return: absolute value
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		return (-a);
+	}
+	else
+	{
+		return (a);
+	}
+}
