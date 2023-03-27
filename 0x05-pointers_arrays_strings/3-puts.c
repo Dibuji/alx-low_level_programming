64f2c75db1cbbeb90c100859fb3a0c4de58494ab
Dibuji <emekzy10@gmail.com>
2023-03-27 12:51:13 -0700

    print to standard output

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - Print to standard output
+ * @str: String value
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	putchar('\n');
+}
