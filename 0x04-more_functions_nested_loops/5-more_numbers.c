042b8b6a7f0d5fd86f42ee04cb6887a8f7f40dde
Dibuji <emekzy10@gmail.com>
2023-04-04 11:02:36 -0700

    printing numbers 0 to 14, 10 times.

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that prints 10 times the numbers,
+ * from 0 to 14
+ *
+ */
+
+void more_numbers(void)
+{
+	int a, b;
+
+	a = 0;
+	while (a < 10)
+	{
+		b = 0;
+		while (b <= 14)
+		{
+			if (b >= 10)
+			{
+				_putchar(b / 10 + '0');
+			}
+			_putchar (b % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
