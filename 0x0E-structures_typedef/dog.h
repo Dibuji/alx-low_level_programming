df91955502f9ef3f4bd199847ee39145e19d038d
Dibuji <emekzy10@gmail.com>
2023-04-17 21:58:53 -0700

    u[date

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct for a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: Struct contains the name, age and owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
+#endif /* DOG_H */
