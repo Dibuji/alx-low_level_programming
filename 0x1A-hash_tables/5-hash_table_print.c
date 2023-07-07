0cd124c6475f8037501d969bdefd35c9c2d8f8f6
Dibuji <emekzy10@gmail.com>
2023-07-07 17:00:47 +0000

    mandatory tasks completed

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -18,19 +18,22 @@ void hash_table_print(const hash_table_t *ht)
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		temp = ht->array[i];
-		
-		while (temp != NULL)
+		if (ht->array[i] != NULL)
 		{
 			if (comma_flag == 1)
 				printf(", ");
 
-			printf("'%s': '%s'", temp->key, temp->value);
-			temp = temp->next;
+			temp = ht->array[i];
 
+			while (temp != NULL)
+			{
+				printf("'%s': '%s'", temp->key, temp->value);
+				temp = temp->next;
+
+				if (temp != NULL)
+					printf(", ");
+			}
 			comma_flag = 1;
-			if (temp != NULL)
-				printf(", ");
 		}
 	}
 	printf("}\n");
